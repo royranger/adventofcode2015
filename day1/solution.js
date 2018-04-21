ca0acc27460d775c37b4aff1f2648f9e77277883
Joy Goh-Mah <royran9er@gmail.com>
2018-04-21 04:19:17 +0100

    completes solution

--- day1/solution.js
+++ day1/solution.js
@@ -12,34 +12,23 @@ const splitIntoArray = (text) => {
 const data = splitIntoArray(getText('./puzzleinput.txt'));
 
 let floor = 0;
+const arrayOfBasementIndexes = [];
 
-data.forEach((character) => {
+
+data.forEach((character, index) => {
   if (character === '(') {
     floor++;
   } else if (character === ')') {
     floor--;
   }
-});
 
-console.log(`Part 1: The floor is ${floor}.`);
-
-
-// Part 2
-
-let floorPartTwo = 0;
-const arrayOfBasementIndexes = [];
-
-data.forEach((character, index) => {
-  if (character === '(') {
-    floorPartTwo++;
-  } else if (character === ')') {
-    floorPartTwo--;
-  }
-
-  if (floorPartTwo === -1) {
+  if (floor === -1) {
     arrayOfBasementIndexes.push(index);
   }
 });
 
 const position = arrayOfBasementIndexes[0] + 1;
+
+
+console.log(`Part 1: The floor is ${floor}.`);
 console.log(`Part 2: He goes to the basement at position ${position}.`);
